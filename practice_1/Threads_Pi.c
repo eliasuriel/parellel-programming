200f6f18c0ce0ce849eb86df003fe6fb207f94cc
A01639716 <83232239+A01639716@users.noreply.github.com>
2022-10-08 15:13:06 -0500

    Update Threads_Pi.c

--- practice_1/Threads_Pi.c
+++ practice_1/Threads_Pi.c
@@ -1,25 +1,31 @@
+/*
 
+Elías Uriel Velázquez Rojas_A01639716
+
+
+*/
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
-#define NUM_THREADS 1
+#define NUM_THREADS 75
 
 int npoints = 100000;
 int cont_circle = 0;
 double radio=1;
+
 pthread_mutex_t lock;
 int pointsThread;
 
 unsigned int seed;
 
-double random(){
+double Numrandom(){
     return (double)rand_r(&seed) / (double)((unsigned)RAND_MAX + 1);
 }
 
-int find_circle(double x, double y){
+int en_circulo(double x, double y){
     double d = pow(radio,2) - (pow(x,2) + pow(y,2));
     if(d>=0){
         return 1;
@@ -28,18 +34,18 @@ int find_circle(double x, double y){
     }
 }
 
-void * Count_montecarlo(void *t_id){
-    double x, y;
+void * Count_montecarlo(void * a){
     int contador = 0;
     pthread_mutex_lock(&lock);
-    for(int i=0; i<pointsThread; i++){
-        x = random();
-        y = random();
-        int circulo = find_circle(x,y);
-        if( circulo ==1){
+    int j;
+    for( j=0; j<pointsThread; j++){
+        double x = Numrandom();
+        double y = Numrandom();
+        int circulo = en_circulo(x,y);
+        if(circulo ==1){
             cont_circle= cont_circle + 1;
         }else{
-          contador = contador + 1;
+          contador++;
         }
     }
      pthread_mutex_unlock(&lock);
@@ -61,13 +67,13 @@ int main(int argc, char *argv[]){
     pthread_t threads[NUM_THREADS];
     pointsThread= npoints/NUM_THREADS;
 
-
-    for(int i=0; i<NUM_THREADS; i++){
-        pthread_create(&threads[i], NULL, contador, NULL);
+    int i;
+    for( i=0; i<NUM_THREADS; i++){
+        pthread_create(&threads[i], NULL, Count_montecarlo, NULL);
     }
-
-    for(int i=0; i<NUM_THREADS; i++){
-        pthread_join(threads[i], NULL);
+    int a;
+    for(a=0; a<NUM_THREADS; a++){
+        pthread_join(threads[a], NULL);
     }
 
     double PI = 4.0*(double)cont_circle/(double)npoints;
